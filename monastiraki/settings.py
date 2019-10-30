3e5b1739f56d23670764b994fd43a09bde82cf4e
Zefarak <lirageika@hotmai.gr>
2019-10-30 20:11:03 +0200

    forgot d de def

--- monastiraki/settings.py
+++ monastiraki/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 USE_REAL_DB = True
 USE_CACHE = True
 PRODUCTION = True
